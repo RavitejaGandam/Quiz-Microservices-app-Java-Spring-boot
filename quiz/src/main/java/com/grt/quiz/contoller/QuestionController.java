e180484494062b889c605c004122cf3fd07edffd
Raviteja Gandam <gandamraviteja@gmail.com>
2024-05-20 15:49:55 +0530

    Add questions API Added

--- quiz/src/main/java/com/grt/quiz/contoller/QuestionController.java
+++ quiz/src/main/java/com/grt/quiz/contoller/QuestionController.java
@@ -3,10 +3,8 @@ package com.grt.quiz.contoller;
 import com.grt.quiz.entity.QuestionEntity;
 import com.grt.quiz.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,4 +24,10 @@ public class QuestionController {
     public List<QuestionEntity> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
     }
+
+    @PostMapping("/addQuestion")
+    public ResponseEntity<QuestionEntity> addQuestion(@RequestBody QuestionEntity questionEntity){
+         return ResponseEntity.ok().body(questionService.addQuestion(questionEntity));
+    }
+
 }
--- quiz/src/main/java/com/grt/quiz/service/QuestionService.java
+++ quiz/src/main/java/com/grt/quiz/service/QuestionService.java
@@ -3,6 +3,7 @@ package com.grt.quiz.service;
 import com.grt.quiz.entity.QuestionEntity;
 import com.grt.quiz.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,5 +20,9 @@ public class QuestionService {
 
     public List<QuestionEntity> getQuestionsByCategory(String category) {
         return questionRepository.findByCategory(category);
+    }
+
+    public QuestionEntity addQuestion(QuestionEntity questionEntity) {
+        return questionRepository.save(questionEntity);
     }
 }
